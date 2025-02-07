38e711474467355150bb83d1f8ccecbb9085b3bd
Vigitoreanec <chirik39@mail.ru>
2025-02-07 14:41:33 +0200

    1) admin.posts -> CRUD Поста 2) index для CRUD 3) Поправили меню для Админа menu

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.app')
 
-@section('title', 'Админ | Категории')
+@section('title', 'Админ | Посты')
 
 @section('menu')
 @include('parts.menu')
+
 @endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Категории</div>
+                <div class="card-header">Посты</div>
 
                 <div class="card-body">
-                    <a href="" class="btn btn-success">создать Категорию </a>
+                    <a href="" class="btn btn-success">создать Пост </a>
 
-                    <h2>CRUD категории</h2>
+                    <h2>CRUD поста</h2>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
--- resources/views/parts/menu.blade.php
+++ resources/views/parts/menu.blade.php
@@ -1,10 +1,13 @@
 <ul class="navbar-nav me-auto">
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('posts.index') }}">Посты</a>
+        <a class="nav-link @if (Route::is('admin.posts')) active @endif" href="{{ route('admin.posts') }}">Посты</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('admin.categories') }}">Категории</a>
     </li>
     @auth()
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('admin.index') }}">Админка</a>
-        </li>
+    <li class="nav-item">
+        <a class="nav-link @if (Route::is('admin.index')) active @endif" href="{{ route('admin.index') }}">Админка</a>
+    </li>
     @endauth
 </ul>

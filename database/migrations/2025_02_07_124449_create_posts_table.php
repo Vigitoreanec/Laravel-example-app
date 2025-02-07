83e7ca854b30243fd7957c5bf0604afc8aeca44e
Vigitoreanec <chirik39@mail.ru>
2025-02-07 15:12:21 +0200

    1) Добавили поля для Постов php artisan make:migration create_posts_table 2) Добавили в таблицу данные php artisan migrate 3) factory()->create для добавления записей 4) php artisan make:seeder PostSeeder Контроллер для ЗАСЕВА 5) function run() добавили функцию 6) php artisan migrate:refresh --seed обновили старое и накатили новое

--- database/migrations/2025_02_07_124449_create_posts_table.php
+++ database/migrations/2025_02_07_124449_create_posts_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable(false);
+            $table->text('text')->nullable(true);
             $table->timestamps();
         });
     }

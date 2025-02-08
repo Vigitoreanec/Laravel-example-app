5b037e79c53105282810100d0a90e00204a87ccb
Vigitoreanec <chirik39@mail.ru>
2025-02-08 10:38:01 +0200

    Правка панели при регистрации

--- resources/views/parts/menu.blade.php
+++ resources/views/parts/menu.blade.php
@@ -1,16 +1,12 @@
 <ul class="navbar-nav me-auto">
-    {{-- @auth()
-    <li class="nav-item">
-        <a class="nav-link @if (Route::is('admin.posts')) active @endif" href="{{ route('admin.posts') }}">Посты</a>
-    </li>
-    @endauth --}}
     <li class="nav-item">
         <a class="nav-link @if (Route::is('posts.index')) active @endif" href="{{ route('posts.index') }}">Посты</a>
     </li>
+
+    @auth()
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.categories') }}">Категории</a>
     </li>
-    @auth()
     <li class="nav-item">
         <a class="nav-link @if (Route::is('admin.index')) active @endif" href="{{ route('admin.index') }}">Админка</a>
     </li>
